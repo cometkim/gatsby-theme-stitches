e7afaab5d9f1d50fbe115b65b446148406b12d82
Hyeseong Kim <hey@hyeseong.kim>
2021-09-03 00:22:24 +0900

    stitches v1 (#8)
    * prepare major
    
    * migrated stitches API
    See https://stitches.dev/blog/migrating-from-beta-to-v1
    
    * cleanup manifest file
    
    * BREAKING CHANGE: rename stitches.config to config

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,22 @@
-import { createCss } from '@stitches/react';
+import { createStitches } from '@stitches/react';
 
 /**
  * You can override this config by component shadowing
- * See https://www.gatsbyjs.com/docs/how-to/plugins-and-themes/shadowing/
+ * @See https://www.gatsbyjs.com/docs/how-to/plugins-and-themes/shadowing/
  *
- * You should export all those three properties `styled`, `css` and `getCssString`.
+ * You should export all those properties.
+ * @See https://stitches.dev/docs/installation#configure-stitches
  */
-export const { styled, css, getCssString } = createCss({
+export const {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  config,
+} = createStitches({
   prefix: '',
   theme: {
     colors: {},
